9ac158c8815987fce3e7384bfad88e7b736a288b
Damas <uint16@users.noreply.github.com>
2020-02-19 20:57:40 +0000

    initial spring project

---
+++ src/main/java/com/stock/market/Interview190079763/Interview190079763Application.java
@@ -0,0 +1,13 @@
+package com.stock.market.Interview190079763;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Interview190079763Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Interview190079763Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/stock/market/Interview190079763/Interview190079763ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.market.Interview190079763;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Interview190079763ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

8f9e203de0b385902936665a48e640fba3c7cb12
Damas <uint16@users.noreply.github.com>
2020-02-21 10:09:13 +0000

    api documentation

--- src/main/java/com/stock/market/Interview190079763/controllers/StockMarketController.java
+++ src/main/java/com/stock/market/Interview190079763/controllers/StockMarketController.java
@@ -2,10 +2,12 @@ package com.stock.market.Interview190079763.controllers;
 
 import com.stock.market.Interview190079763.exception.StockMarketException;
 import com.stock.market.Interview190079763.models.StockType;
+import com.stock.market.Interview190079763.models.Trade;
 import com.stock.market.Interview190079763.models.TradeDirection;
 import com.stock.market.Interview190079763.services.CommonStockCalculationEngine;
 import com.stock.market.Interview190079763.services.PreferredStockCalculationEngine;
 import com.stock.market.Interview190079763.services.TradeService;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/stock")
@@ -26,6 +31,7 @@ public class StockMarketController {
     @Autowired
     private TradeService tradeService;
 
+    @ApiOperation(value = "Calculate dividend yield")
     @GetMapping(value = "/dividendYield/{stockSymbol}")
     public BigDecimal getDividendYield(@PathVariable String stockSymbol, @RequestParam StockType stockType, @RequestParam(required = true) BigDecimal price) throws StockMarketException {
         logger.info("Requested Dividend Yield for Stock " + stockSymbol + " for price " + price);
@@ -39,26 +45,37 @@ public class StockMarketController {
         return BigDecimal.ZERO;
     }
 
+    @ApiOperation(value = "Calculate profit to earnings ratio")
     @GetMapping(value = "/profitToEarningRatio")
     public BigDecimal getProfitToEarningRatio(@RequestParam String ticker, @RequestParam(required = true) BigDecimal price) {
 
         return commonStockCalculationEngine.profitToEarningsRatio(ticker, price);
     }
 
+    @ApiOperation(value = "Perform a trade on a stock")
     @GetMapping(value = "/trade")
     public HttpStatus tradeStock(@RequestParam String ticker, @RequestParam TradeDirection tradeDirection, @RequestParam(required = true) BigDecimal price, @RequestParam BigDecimal quantity) {
         tradeService.recordTrade(ticker, quantity, price, tradeDirection);
         return HttpStatus.OK;
     }
 
+    @ApiOperation(value = "Calculate the GBCE All Share Index using the geometric mean of prices for all stocks")
     @GetMapping(value = "/geometricMean")
     public BigDecimal getGeometricMean() {
         return commonStockCalculationEngine.geometricMean();
 
     }
 
+    @ApiOperation(value = "Calculate Volume Weighted Stock Price based on trades in past 15 minutes")
     @GetMapping(value = "/volumeWeightedStockPrice")
     public BigDecimal getVolumeWeightedStockPrice(@RequestParam String ticker) {
         return commonStockCalculationEngine.volumeWeighedStockPrice(ticker);
+    }
+
+
+    @ApiOperation(value = "FOR TESTING ONLY - See all trades done during session")
+    @GetMapping(value = "/viewTrades")
+    public List<Trade> xGetAllTrades(@RequestParam Optional<String> ticker, @RequestParam Optional<LocalDateTime> from, @RequestParam Optional<LocalDateTime> to) {
+        return tradeService.getAllTradeRecords(from, to, ticker);
     }
 }
